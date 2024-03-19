775ba7d47b537b02c1b9c500f12874823d500b93
unknown <dixondaniel434@gmail.com>
2024-03-19 21:34:37 +0200

    Sticky note and Background

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   """A basic route handler that returns a greeting."""
-  return render_template("index.html", message="Bro this is flask")
+  return render_template("index.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
