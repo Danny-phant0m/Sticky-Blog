a8b6d5724cf003b47c8820db8a3014daf09840d6
unknown <dixondaniel434@gmail.com>
2024-03-19 15:28:30 +0200

    Starting Point for sticky blog

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+  """A basic route handler that returns a greeting."""
+  return render_template("index.html", message="Bro this is flask")
+
+if __name__ == "__main__":
+  app.run(debug=True)
+
