6e5a3e62e669ffd8d7aad67f9cc6b630123eb7bf
unknown <dixondaniel434@gmail.com>
2024-03-21 01:19:24 +0200

    Login and SignUp page

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def start():
+def home():
   return render_template("index.html")
+
+@app.route("/login")
+def login():
+  return render_template("login.html")
+
+@app.route("/SignUp")
+def signUp():
+  return render_template("signup.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
